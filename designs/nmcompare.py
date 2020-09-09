64b01f093f72c5851a1412a5518c3a838fe3d36e
Jacob North <microversallaboratories@gmail.com>
2020-09-09 07:22:15 -0700

    comparison and tf-gan example (from GOOGLE)

---
+++ designs/nmcompare.py
@@ -0,0 +1,11 @@
+
+
+def nmcompare_diff(nm1, nm2):
+    # for each mode in 1, 
+        # for each mode in 2,
+            # minimize RMSD between the angular ensemble (set of all angles, i.e. minimize the dot products between all compared vectors)
+            # append the sum of dot products to a tracker list
+    # rank and output the comparison between the vector sets
+
+def nmcompare_same(nm1, nm2):
+    # for each mode in the structure
